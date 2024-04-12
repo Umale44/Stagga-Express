21f17efc3cfbc43203e3d1e07e1dca0696834ab2
Unopa Male <cse-057@thuto.bac.ac.bw>
2024-04-12 20:06:21 +0200

    DATABASE TEST 1

---
+++ Staggaconnect.session.sql
@@ -0,0 +1,4 @@
+
+
+INSERT INTO dealsoftheweek (image_name, image_data, description)
+VALUES ('braii test.png', LOAD_FILE("C:\Users\cse22-057\RnI Website\Stagga-Express\braii test.png"), 'Description of image 1');
